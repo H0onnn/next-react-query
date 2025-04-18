1d4d5576ceed695a9f48edf2d44304c1ee9d9cf1
H0onnn <dukei2017@naver.com>
2025-04-18 13:17:07 +0900

    feat: sonner Toaster 추가

--- shared/providers/index.ts
+++ shared/providers/index.ts
@@ -1,1 +1,2 @@
 export { default as Providers } from "./providers";
+export { Toaster } from "./toaster";
--- shared/providers/providers.tsx
+++ shared/providers/providers.tsx
@@ -1,12 +1,18 @@
 import { type ReactNode } from "react";
 import QueryProvider from "./query-provider";
+import { Toaster } from "./toaster";
 
 type ProvidersProps = {
   children: ReactNode;
 };
 
 const Providers = ({ children }: ProvidersProps) => {
-  return <QueryProvider>{children}</QueryProvider>;
+  return (
+    <QueryProvider>
+      {children}
+      <Toaster richColors={true} />
+    </QueryProvider>
+  );
 };
 
 export default Providers;
